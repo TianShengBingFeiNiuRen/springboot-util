e7b410eba102365d88c0c0bea765597422f7273d
Andon <17610835229@163.com>
2021-12-29 14:22:46 +0800

    WebMvcConfigurer

---
+++ src/main/java/com/andon/springbootutil/config/WebMvcConfigurer.java
@@ -0,0 +1,28 @@
+package com.andon.springbootutil.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author Andon
+ * 2021/12/29
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurationSupport {
+
+    /**
+     * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。 需要重新指定静态资源
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html", "doc.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+
+}
