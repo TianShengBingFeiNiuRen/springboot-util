83e28de32608ac704cbe0959e200a5388626f548
Andon <17610835229@163.com>
2022-04-15 11:27:44 +0800

    多环境、额外 配置文件

---
+++ src/main/java/com/andon/springbootutil/config/AndonAddConfig.java
@@ -0,0 +1,20 @@
+package com.andon.springbootutil.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Andon
+ * 2022/4/15
+ */
+@Data
+@Component
+@PropertySource({"classpath:andon.add.properties"})
+@ConfigurationProperties(prefix = "andon.add")
+public class AndonAddConfig {
+
+    private Boolean enable = Boolean.FALSE;
+    private String name;
+}
