35fa178575795a81e719736178fc3d638facbff0
Andon <17610835229@163.com>
2022-05-27 17:48:41 +0800

    FileUtilTest

--- src/test/java/com/andon/springbootutil/FileUtilTest.java
+++ src/test/java/com/andon/springbootutil/FileUtilTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 /**
  * @author Andon
@@ -19,6 +19,68 @@ import java.util.concurrent.CompletableFuture;
  */
 @Slf4j
 public class FileUtilTest {
+
+    @Test
+    public void appendContentToFile2() throws Exception {
+        File file = FileUtil.createFile("createFile.csv");
+        List<String> header = Collections.singletonList("id,name,value");
+        FileUtil.appendContentToFile(file, header);
+        int total = 2000_0000;
+        int row = 100_0000;
+        int shard = total / row;
+
+        Semaphore semaphore = new Semaphore(50);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < shard; i++) {
+            semaphore.acquire();
+            int finalI = i;
+            Future<?> future = executorService.submit(() -> {
+                try {
+                    List<String> lines = new ArrayList<>(row);
+                    for (int i1 = 0; i1 < row; i1++) {
+                        lines.add(finalI + "," + Thread.currentThread().getName() + "," + i1 + UUID.randomUUID().toString());
+                    }
+                    FileUtil.appendContentToFile(file, lines);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            });
+            futures.add(future);
+        }
+        for (Future<?> future : futures) {
+            future.get();
+        }
+    }
+
+    @Test
+    public void readFileContentByLine2() throws Exception {
+        String filePath = "D:\\apps\\file\\createFile.csv";
+        int batchSize = 10_0000;
+        List<String> lines = new ArrayList<>(batchSize);
+        FileUtil.readFileContentByLine(filePath, (bufferedReader) -> {
+            String line;
+            int shard = 0;
+            while (true) {
+                try {
+                    if ((line = bufferedReader.readLine()) == null) break;
+                } catch (Exception e) {
+                    break;
+                }
+                lines.add(line);
+                if (lines.size() == batchSize) {
+                    log.info("shard:{} - lines.size={}", shard += 1, lines.size());
+                    lines.clear();
+                }
+            }
+            if (!lines.isEmpty()) {
+                log.info("shard:{} - lines.size={}", shard += 1, lines.size());
+                lines.clear();
+            }
+        });
+    }
 
     @Test
     public void appendContentToFile() throws IOException {
