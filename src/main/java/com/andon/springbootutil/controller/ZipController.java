1ba09cd36f9f89fdc571d7449f84c9a35cfaaf92
Andon <17610835229@163.com>
2022-09-08 19:20:24 +0800

    Update

--- src/main/java/com/andon/springbootutil/controller/ZipController.java
+++ src/main/java/com/andon/springbootutil/controller/ZipController.java
@@ -7,7 +7,9 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -25,7 +27,7 @@ public class ZipController {
 
     @ApiOperation("压缩下载")
     @GetMapping(value = "/zip-download")
-    public void zipDownload(String zipFileName, HttpServletResponse httpServletResponse) {
+    public void zipDownload(@RequestParam String zipFileName, @ApiIgnore HttpServletResponse httpServletResponse) {
         zipFileName = String.format("%s_%s.zip", zipFileName, System.currentTimeMillis());
         String inputFilePath = "F:\\安恒\\MPC\\MPC 1.0.3\\test";
         File zipFile = ZipUtil.zipFilePath(inputFilePath, zipFileName);
