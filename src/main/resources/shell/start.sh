1bd8c4548b47f4769326ed5dc73c9f1acd26ea3b
Andon <34623775+TianShengBingFeiNiuRen@users.noreply.github.com>
2023-03-09 11:02:10 +0800

    start.sh

--- src/main/resources/shell/start.sh
+++ src/main/resources/shell/start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 source /etc/profile
-cd /data/springboot-util
-./stop.sh
 nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8868 -jar -XX:+UseConcMarkSweepGC springboot-util-release.jar >nohup.out 2>&1 &
