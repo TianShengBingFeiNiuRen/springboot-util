38033cf27379ddfa3ab19025e64e569b77c6eecf
Andon <xudong.wang@dbappsecurity.com.cn>
2024-02-04 16:01:06 +0800

    start.sh

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -9,4 +9,7 @@ JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
 
 JVM_REMOTE_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=7766"
 
+echo "$(date +'%Y-%m-%d %H:%M:%S') 等待启动 ${APPLICATION}..."
 nohup java ${JAVA_OPT} ${JVM_REMOTE_OPTS} -jar ./lib/${APPLICATION_JAR} > /dev/null 2>&1 &
+sleep 5s
+echo "$(date +'%Y-%m-%d %H:%M:%S') ${APPLICATION} started..."
