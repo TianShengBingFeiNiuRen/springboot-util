f32cd3ead40c86c972a187e4e2637dac459ca9b2
摸鱼佬 <17610835229@163.com>
2025-04-07 18:45:09 +0800

    start.sh stop.sh

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -4,7 +4,10 @@ source /etc/profile
 APPLICATION="springboot-util"
 APPLICATION_JAR="${APPLICATION}.jar"
 
-workspace=$(pwd)
+workspace=$(cd $(dirname $0); pwd)
+
+LOG_DIR="${workspace}/log"
+mkdir -p ${LOG_DIR}
 
 JAVA_OPT="-server -XX:NewRatio=2 -XX:SurvivorRatio=8 -Xms8g -Xmx16g"
 JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
@@ -12,7 +15,7 @@ JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
 JVM_REMOTE_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=7766"
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') 等待启动 ${APPLICATION}..."
-nohup java ${JAVA_OPT} ${JVM_REMOTE_OPTS} -jar ${workspace}/lib/${APPLICATION_JAR} > /dev/null 2>&1 &
+nohup java ${JAVA_OPT} ${JVM_REMOTE_OPTS} -Dlog_dir=${LOG_DIR} -jar ${workspace}/lib/${APPLICATION_JAR} > /dev/null 2>&1 &
 sleep 5s
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${APPLICATION} started..."
 
--- src/bin/stop.sh
+++ src/bin/stop.sh
@@ -3,7 +3,7 @@
 APPLICATION="springboot-util"
 APPLICATION_JAR="${APPLICATION}.jar"
 
-workspace=$(pwd)
+workspace=$(cd $(dirname $0); pwd)
 
 PID=$(ps -ef | grep "${workspace}/lib/${APPLICATION_JAR}" | grep -v grep | awk '{print $2}')
 kill -9 ${PID}
